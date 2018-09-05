16039726a5418654b5febbd33fa32d979eb0ff58
govindMundhe <govindmundhe452@gmail.com>
2018-09-05 21:36:52 +0530

    Add files via upload
    Swing UI code

---
+++ Notepad.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+
+public class Notepad {
+	
+	JFrame f;
+	JMenuBar m;
+	JMenu file, edit, format, help;
+	JMenuItem open, save, saveAs, copy, cut, paste;
+	JMenuItem contact, about;
+	JTextArea t;
+
+	public Notepad() {
+		// TODO Auto-generated constructor stub
+		f = new JFrame();
+		f.setTitle("Notepad using Swing");
+		m = new JMenuBar();
+		f.setJMenuBar(m);
+		file = new JMenu("File");
+		m.add(file);
+		
+		open = new JMenuItem("Open");
+		file.add(open);
+		save = new JMenuItem("Save");
+		file.add(save);
+		saveAs = new JMenuItem("Save As");
+		file.add(saveAs);
+		file.addSeparator();
+		edit = new JMenu("Edit");
+		m.add(edit);
+		
+		copy = new JMenuItem("Copy");
+		edit.add(copy);
+		cut = new JMenuItem("Cut");
+		edit.add(cut);
+		paste = new JMenuItem("Paste");
+		edit.add(paste);
+		
+		
+		format = new JMenu("Format");
+		m.add(format);
+		
+		help = new JMenu("Help");
+		m.add(help);
+		
+		contact = new JMenuItem("Contact Us");
+		help.add(contact);
+		about = new JMenuItem("About");
+		help.add(about);
+		
+		
+		t = new JTextArea(200,200);
+		f.add(t);
+		
+		f.setVisible(true);
+		f.setSize(1000, 1000);
+	}
+	public static void main(String[] args) {
+		Notepad n = new Notepad();
+	}
+}
